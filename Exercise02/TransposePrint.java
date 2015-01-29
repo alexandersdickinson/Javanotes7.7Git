747762f80339b094b83f8677aa7fac6a55e9f179
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-01-29 08:43:59 -0800

    Updated Exercise02

--- Exercise02/TransposePrint.java
+++ Exercise02/TransposePrint.java
@@ -2,11 +2,37 @@
 	This class prints the transpose of a given int[][].
 */
 
+import java.util.Arrays;
+
 public class TransposePrint{
 
 	public static void main(String[] args){
 		
+		int[][] arr = new int[10][10];
 		
+		for(int i = 0; i < 10; i++){//rows
+			
+			for(int j = 0; j < 10; j++){//columns
+				
+				arr[i][j] = j;
+				
+			}
+				
+		}
+	
+		arr = transpose(arr);
+		
+		for(int[] i:arr){
+			
+			for(int j:i){
+				
+				System.out.print(j + "  ");
+				
+			}
+			
+			System.out.println();
+			
+		}
 		
 	}
 	
@@ -16,8 +42,16 @@ public class TransposePrint{
 	*/
 	static int[][] transpose(int[][] baseArr){
 		
+		int[][] transArr = new int[baseArr.length][];
 		
+		for(int i = 0; i < baseArr.length; i++){//for each row
+			
+			transArr[i] = Arrays.copyOf(baseArr[i], i + 1);
+			
+		}
 		
+		return transArr;
+				
 	}
 
 }
